89cdae23055a71621bce900c82ad7f0a597955d4
gosuljo <lurutia@naver.com>
2021-11-14 00:15:00 +0900

    [ADD] 피보나치 수열

--- inflearn_java_algorithm/14_보이는_학생/Main.java
+++ inflearn_java_algorithm/14_보이는_학생/Main.java
@@ -1,39 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /*
 input
-5
-2 3 3 1 3
-1 1 2 2 3
+10
 
 output
-A
-B
-A
-B
-D
+1 1 2 3 5 8 13 21 34 55
  */
 
 public class Main {
-    public String[] solution(int[] arr, int[] arr2, int n) {
-        String answer[] = new String[n];
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
         
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 1 && arr2[i] == 3) {
-                answer[i] = "A";
-            }
-            else if (arr[i] == 2 && arr2[i] == 1) {
-                answer[i] = "A";
-            }
-            else if (arr[i] == 3 && arr2[i] == 2) {
-                answer[i] = "A";
-            }
-            else if (arr[i] == arr2[i]) {
-                answer[i] = "D";
-            }
-            else {
-                answer[i] = "B";
-            }
+        answer.add(1);
+        answer.add(1);
+
+        for (int i = 2; i < n; i++) {
+            answer.add(answer.get(i-2) + answer.get(i-1));
         }
 
 
@@ -44,17 +29,9 @@ public class Main {
         Main T = new Main();
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
-        int arr[] = new int[n];
-        int arr2[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            arr2[i] = in.nextInt();
-        }
-
-        for (String x : T.solution(arr, arr2, n)) {
-            System.out.println(x);
+       
+        for (int x : T.solution(n)) {
+            System.out.print(x + " ");
         }
 
         return ;
