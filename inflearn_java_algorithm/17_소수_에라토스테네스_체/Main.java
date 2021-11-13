5d05801af188d380bee4c145e5cd120b02f8449f
gosuljo <lurutia@naver.com>
2021-11-14 00:31:35 +0900

    [ADD] 소수(에라토스테네스 체

---
+++ inflearn_java_algorithm/17_소수_에라토스테네스_체/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/*
+input
+20
+
+output
+8
+ */
+
+public class Main {
+    public int solution(int n) {
+        int answer = 0;
+        int arr[] = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+        
+        for (int i = 2; i <= n/2; i++) {
+            for (int j = i*2; j <= n; j=j+i) {
+                arr[j] = 1;
+            }
+        }
+
+        for (int x : arr) {
+            if (x == 0) answer++;
+        }
+
+       return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main T = new Main();
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+       
+        System.out.println(T.solution(n));
+
+        return ;
+    }
+}
