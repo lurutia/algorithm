5fda9131457f4de2f69a6bfb1326e400dfd814ba
gosuljo <lurutia@naver.com>
2021-11-14 15:08:27 +0900

    [ADD] 임시반장 정하기

---
+++ inflearn_java_algorithm/23_임시반장_정하기/Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+/*
+input
+5
+2 3 1 7 3
+4 1 9 6 8
+5 5 2 4 4
+6 5 2 6 7
+8 4 2 2 2
+
+output
+4
+ */
+
+public class Main {
+    public int solution(int[][] arr, int n) {
+        int answer = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer = i+1;
+            }
+        }
+        
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main T = new Main();
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr = new int[n][5];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+       
+        System.out.print(T.solution(arr, n));
+
+        return ;
+    }
+}
