352df69135e445a4d90c295c9d6c527126bcd7fa
gosuljo <lurutia@naver.com>
2021-12-22 00:31:18 +0900

    [ADD] 돌다리 건너기

---
+++ inflearn_java_algorithm/71_돌다리_건너기/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/*
+input
+7
+
+output
+34
+ */
+
+ public class Main {
+    static int answer = 0;
+    public void DFS(int L, int sum, int n) {
+        if (sum >= n) {
+            answer++;
+        }
+        else {
+            DFS(L + 1, sum + 1, n);
+            DFS(L + 1, sum + 2, n);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        T.DFS(0, 0, n);
+        System.out.println(answer);
+
+        return ;
+    }
+}
