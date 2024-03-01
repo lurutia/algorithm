cdf852146cd67936fd465c9ec6795a7e60766fe1
gosuljo <lurutia@naver.com>
2024-03-01 14:44:22 +0900

    [알고리즘 스터디] 웜업 문제 백준 A-B 1001번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/A_Minus_B_1001.java
@@ -0,0 +1,12 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class A_Minus_B_1001 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a - b);
+    }
+}
