d4be7e85990c3e4261f27f869514a727c848356f
gosuljo <lurutia@naver.com>
2024-03-01 14:50:44 +0900

    [알고리즘 스터디] 웜업 문제 백준 사칙연산 10869번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/ArithmeticOperations_10869.java
@@ -0,0 +1,16 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class ArithmeticOperations_10869 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
