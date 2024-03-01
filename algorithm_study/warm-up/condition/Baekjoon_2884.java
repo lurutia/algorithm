1d284e9917d197b64810098758653237fb5e6d05
gosuljo <lurutia@naver.com>
2024-03-01 18:56:12 +0900

    [알고리즘 스터디] 웜업 문제 백준 오븐 시계 2525

--- algorithm_study/warm-up/condition/Baekjoon_2884.java
+++ algorithm_study/warm-up/condition/Baekjoon_2884.java
@@ -8,15 +8,14 @@ public class Baekjoon_2884 {
         Scanner s = new Scanner(System.in);
         int h = s.nextInt();
         int m = s.nextInt();
+        int cookMinute = s.nextInt();
 
-        solution(h, m);
+        solution(h, m, cookMinute);
     }
 
-    private static void solution(int h, int m) {
-        int result = 0;
-
+    private static void solution(int h, int m, int cookMinute) {
         LocalDateTime t1 = LocalDateTime.of(2024, 1, 1, h, m, 0);
-        LocalDateTime t2 = t1.plusMinutes(-45);
+        LocalDateTime t2 = t1.plusMinutes(cookMinute);
 
         System.out.println(t2.getHour() + " " + t2.getMinute());
     }
