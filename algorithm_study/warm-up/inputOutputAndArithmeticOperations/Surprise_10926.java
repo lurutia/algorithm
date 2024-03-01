80a6e2574075ce63f85f572ebb72ab17b350bd3f
gosuljo <lurutia@naver.com>
2024-03-01 14:55:25 +0900

    [알고리즘 스터디] 웜업 문제 백준 ??! 10926번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/Surprise_10926.java
@@ -0,0 +1,11 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class Surprise_10926 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String id = s.nextLine();
+        System.out.println(id + "??!");
+    }
+}
