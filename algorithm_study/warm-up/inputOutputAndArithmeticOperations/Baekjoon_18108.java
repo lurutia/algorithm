03479b94630535640f74f31d0f08791d632b5ac2
gosuljo <lurutia@naver.com>
2024-03-01 14:58:40 +0900

    [알고리즘 스터디] 웜업 문제 백준 1998년생인 내가 태국에서는 2541년생?! 18108번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/Baekjoon_18108.java
@@ -0,0 +1,11 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class Baekjoon_18108 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int inputYear = s.nextInt();
+        System.out.println(inputYear - 543);
+    }
+}
