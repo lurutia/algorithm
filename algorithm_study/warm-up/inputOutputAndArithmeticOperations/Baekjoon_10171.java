02d37c8cbaebe78fcbe6f8e5a413998bd67298a4
gosuljo <lurutia@naver.com>
2024-03-01 15:14:51 +0900

    [알고리즘 스터디] 웜업 문제 백준 고양이 10171번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/Baekjoon_10171.java
@@ -0,0 +1,16 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class Baekjoon_10171 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    private static void solution() {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
