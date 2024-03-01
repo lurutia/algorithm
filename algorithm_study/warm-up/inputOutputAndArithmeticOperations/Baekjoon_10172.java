0eed9c897e25406da60373a314dff8400ec1e551
gosuljo <lurutia@naver.com>
2024-03-01 15:16:06 +0900

    [알고리즘 스터디] 웜업 문제 백준 개 10172번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/Baekjoon_10172.java
@@ -0,0 +1,15 @@
+package inputOutputAndArithmeticOperations;
+
+public class Baekjoon_10172 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    private static void solution() {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
