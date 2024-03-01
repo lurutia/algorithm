384174bf09843fcf3c44489d15d5b09d48c8113f
gosuljo <lurutia@naver.com>
2024-03-01 14:45:40 +0900

    [알고리즘 스터디] 웜업 문제 백준 A*B 10998번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/A_multiply_B_10998.java
@@ -0,0 +1,12 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class A_multiply_B_10998 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a * b);
+    }
+}
