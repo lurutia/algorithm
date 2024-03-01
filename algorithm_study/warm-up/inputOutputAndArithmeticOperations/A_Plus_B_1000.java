6355eca1a8449fa656494f8f67324fcf6958a4e4
gosuljo <lurutia@naver.com>
2024-03-01 14:42:52 +0900

    [알고리즘 스터디] 웜업 문제 백준 A+B 1000번

---
+++ algorithm_study/warm-up/inputOutputAndArithmeticOperations/A_Plus_B_1000.java
@@ -0,0 +1,12 @@
+package inputOutputAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class A_Plus_B_1000 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+    }
+}
