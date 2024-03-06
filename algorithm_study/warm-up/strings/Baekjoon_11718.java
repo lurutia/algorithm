7b35463c927bf8fd2ca7ff6eba952eca0153c3be
gosuljo <lurutia@naver.com>
2024-03-06 23:39:21 +0900

    [알고리즘 스터디] 웜업 문제 킹, 퀸, 룩, 비숍, 나이트, 폰 3003번

--- algorithm_study/warm-up/strings/Baekjoon_11718.java
+++ algorithm_study/warm-up/strings/Baekjoon_11718.java
@@ -1,21 +1,33 @@
 package strings;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Baekjoon_11718 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        solution();
+        int n = 6;
+        List<Integer> list = new ArrayList<>();
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.valueOf(input[i]));
+        }
+        solution(n, list);
     }
 
-    private static void solution() throws IOException {
+    private static void solution(int n, List<Integer> list) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("         ,r'\"7\n" +
-                "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +
-                "   `~\\/\n" +
-                "      |\n" +
-                "      |");
+        Integer[] base = new Integer[]{1, 1, 2, 2, 2, 8};
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            answer.add(base[i] - list.get(i));
+        }
+        bw.write(answer
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" ")));
         bw.flush();
     }
 }
